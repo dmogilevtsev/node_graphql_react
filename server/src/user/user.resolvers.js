cc20b67370804561a1799b83905180b8093515de
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2021-11-23 16:40:34 +0500

    change args and add subscription

--- server/src/user/user.resolvers.js
+++ server/src/user/user.resolvers.js
@@ -1,23 +1,33 @@
 import { UserService } from './user.service'
+import { pubSub } from '../graphql/pubSub'
+
+const NEW_USER = 'NEW_USER'
 
 export const userResolvers = {
-	Query: {
-		async getAllUsers() {
-			return await UserService.getAllUsers()
-		},
-		async getUserById(_, id) {
-			return await UserService.getUserById(id)
-		},
-		async getUserByEmail(_, email) {
-			return await UserService.getUserByEmail(email)
-		},
-	},
-	Mutation: {
-		async createUser(_, user) {
-			return await UserService.createUser(user)
-		},
-		async removeUser(_, id) {
-			return await UserService.removeUser(id)
-		},
-	},
+  Query: {
+    async getAllUsers() {
+      return await UserService.getAllUsers()
+    },
+    async getUserById(_, { id }) {
+      return await UserService.getUserById(id)
+    },
+    async getUserByEmail(_, { email }) {
+      return await UserService.getUserByEmail(email)
+    },
+  },
+  Mutation: {
+    async createUser(_, { user }) {
+      const newUser = await UserService.createUser(user)
+      pubSub.publish(NEW_USER, { newUser })
+      return newUser
+    },
+    async removeUser(_, { id }) {
+      return await UserService.removeUser(id)
+    },
+  },
+  Subscription: {
+    newUser: {
+      subscribe: () => pubSub.asyncIterator([NEW_USER]),
+    },
+  },
 }
