51458026ee58e578fbbb03cc196162f94e9a31b2
Dmitry Mogilevtsev <dmitry.m@intspirit.com>
2021-11-24 19:06:58 +0300

    client: - add onClose method to toast

--- client/src/components/notifications/notification/Notification.tsx
+++ client/src/components/notifications/notification/Notification.tsx
@@ -14,7 +14,7 @@ const Notification = ({toast}: any) => {
     }, [])
     return (
         <div className="my-1 pe-2" style={{zIndex: 999}}>
-            <Toast bg={toast.bg} className="ms-auto" delay={3000} show={isShow}>
+            <Toast bg={toast.bg} className="ms-auto" delay={3000} show={isShow} onClose={() => setIsShow(false)}>
                 <Toast.Header>
                     <strong className="me-auto">{toast.title}</strong>
                 </Toast.Header>
